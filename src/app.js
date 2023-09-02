a72b16c774b81e491cbeb76093c83719b2b599c2
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-09-02 22:33:46 +0300

    1- Implement some global middlewares for enhanced security, including protection against XSS, NoSQL query injection, and parameter pollution attacks 2- Implement CORS middleware to enforce cross-origin policies and apply rate limiting for protection against DOS attacks 3-  Introduce a placeholder for the payment webhook functionality.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const morgan = require('morgan');
 const path = require('path');
+const morgan = require('morgan');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const hpp = require('hpp');
+const xss = require('xss-clean');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+
+const { isLogin } = require('./controllers/authController');
+const globalErrorHandler = require('./controllers/controllerUtils/errorController');
 
 const authRouter = require('./routes/auth.Routes');
 const usersRouter = require('./routes/users.Routes');
@@ -11,18 +21,36 @@ const coursesRouter = require('./routes/courses.Routes');
 const reviewsRouter = require('./routes/reviews.Routes');
 const friendsRouter = require('./routes/friends.Routes');
 const meetingsRouter = require('./routes/meetings.Routes');
-
-const { isLogin } = require('./controllers/authController');
-const globalErrorHandler = require('./controllers/controllerUtils/errorController');
 //--------------------------------//
 const app = express();
-
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+app.use(cors());
+app.options('*', cors());
+
+// Set security HTTP headers
+// app.use(helmet.contentSecurityPolicy({}));
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
+// Payment webhook, BEFORE body-parser, because it needs the body as stream
+// app.post('/webhook-checkout', bodyParser.raw({ type: 'application/json' }));
 
 app.use(express.json({ limit: '10kb' }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, '..', 'public')));
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+// app.use(hpp());
 //--------------------------------//
 app.use('/api/v1/auth', authRouter);
 app.use(isLogin);
