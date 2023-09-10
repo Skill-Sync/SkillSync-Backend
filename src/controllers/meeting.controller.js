373b64f47917added9865090e8db2214f5997075
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-09-10 19:30:33 +0300

    get my meetings endpoint is created to return just the schedules meetings for either user or mentor to be shown in the home page

--- src/controllers/meeting.controller.js
+++ src/controllers/meeting.controller.js
@@ -3,7 +3,26 @@ const AppError = require('../utils/appErrorsClass');
 const factory = require('./controllerUtils/handlerFactory');
 const catchAsyncError = require('../utils/catchAsyncErrors');
 // ------------- User Operations ------------//
-exports.getMyMeetings = catchAsyncError(async (req, res, next) => {});
+exports.getMyMeetings = catchAsyncError(async (req, res, next) => {
+  const userId = res.locals.userId;
+  let meetings;
+  if (res.locals.userType === 'mentor') {
+    meetings = await Meeting.find({
+      mentor: userId,
+      state: { $ne: 'not-selected' }
+    });
+  } else {
+    meetings = await Meeting.find({ user: userId });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    results: meetings.length,
+    data: {
+      meetings
+    }
+  });
+});
 exports.updateMeeting = catchAsyncError(async (req, res, next) => {});
 exports.createMeeting = catchAsyncError(async (req, res, next) => {});
 // ---------- Basic CRUD Operations ----------//
