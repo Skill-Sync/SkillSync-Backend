021b09339712cb6a241a081ea3491520469ffa30
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-09-11 22:35:16 +0300

    removing skills from auth protected routes

--- src/routes/skill.routes.js
+++ src/routes/skill.routes.js
@@ -1,17 +1,20 @@
 const express = require('express');
+const { isLogin } = require('../controllers/auth.controller');
 const authController = require('../controllers/auth.controller');
 const skillController = require('../controllers/skill.controller.js');
+const app = require('../app');
 //-----------------------------------------//
 const router = express.Router();
 //-------------------Router----------------//
 router
-    .get('/', skillController.getAllSkills)
-    .get('/:id', skillController.getSkill);
+  .get('/', skillController.getAllSkills)
+  .get('/:id', skillController.getSkill);
 
+app.use(isLogin);
 router.use(authController.restrictTo('admin'));
 router
-    .post('/', skillController.createSkill)
-    .patch('/:id', skillController.updateSkill)
-    .delete('/:id', skillController.deleteSkill);
+  .post('/', skillController.createSkill)
+  .patch('/:id', skillController.updateSkill)
+  .delete('/:id', skillController.deleteSkill);
 //-------------------------------------------//
 module.exports = router;
