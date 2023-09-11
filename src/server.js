70206e53e536d3582d738658e308e13e0f11297b
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-09-11 16:07:10 +0300

    Resolving SIGTERM issue 1.0

--- src/server.js
+++ src/server.js
@@ -44,9 +44,9 @@ process.on('unhandledRejection', err => {
         process.exit(1);
     });
 });
-// process.on('SIGTERM', () => {
-//   console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
-//   server.close(() => {
-//     console.log('💥 Process terminated!');
-//   });
-// });
+process.on('SIGTERM', () => {
+    console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+    // server.close(() => {
+    //     console.log('💥 Process terminated!');
+    // });
+});
