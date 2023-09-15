8cd3b8e70e93236bca0e7bfe78397e01bb0c37a7
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-09-15 17:09:20 +0300

    Integrating with dyte 1.2

--- src/models/mentor.model.js
+++ src/models/mentor.model.js
@@ -1,124 +1,35 @@
-const bcrypt = require('bcryptjs');
-const crypto = require('crypto');
 const mongoose = require('mongoose');
-const validator = require('validator');
-//------------------------------------------//
-const mentorSchema = new mongoose.Schema(
-    {
-        role: {
-            type: String,
-            default: 'mentor'
-        },
-        name: {
-            type: String,
-            required: [true, 'A user must have a name']
-        },
-        email: {
-            type: String,
-            unique: true,
-            lowercase: true,
-            validate: {
-                validator: validator.isEmail,
-                message: 'Please provide a valid email'
-            },
-            required: [true, 'A user must have an email']
-        },
-        pass: {
-            type: String,
-            required: [true, 'A user must have a password'],
-            minlength: 8,
-            select: false
-        },
-        passConfirm: {
-            type: String,
-            validate: {
-                validator: function(el) {
-                    return el === this.pass;
-                },
-                message: 'Passwords are not the same!'
-            },
-            required: [true, 'A user must have a password confirmation']
-        },
-        identityCard: {
-            type: String
-        },
-        photo: {
-            type: String,
-            default: 'default.jpg'
-        },
-        about: {
-            type: String,
-            default: 'No description'
-        },
-        skill: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Skill'
-        },
-        requestLetter: {
-            type: String,
-            trim: true,
-            default: 'No request letter'
-        },
-        onboarding_completed: {
-            type: Boolean,
-            default: true
-        },
-        isVerified: {
-            type: Boolean,
-            default: false
-        },
-        active: {
-            type: Boolean,
-            default: false,
-            select: false
-        },
-        workHoursRange: {
-            type: String,
-            default: '9:00-14:00'
-        },
-        passwordResetToken: String,
-        passwordResetExpires: Date
+//-------------------Schema----------------//
+const meetingsSchema = new mongoose.Schema({
+    mentor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Mentor'
     },
-    {
-        toJSON: { virtuals: true },
-        toObject: { virtuals: true }
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    dyteMeetingId: {
+        type: String
+    },
+    status: {
+        type: String,
+        enum: ['not-selected', 'pending', 'accepted', 'rejected'],
+        default: 'not-selected'
+    },
+    scheduledDate: {
+        type: Date,
+        required: [true, 'A meeting must have a scheduled date']
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
     }
-);
-//-------------------Instance Methods-------------------//
-mentorSchema.methods.correctPassword = async function(loginPass, userPass) {
-    return await bcrypt.compare(loginPass, userPass);
-};
-
-mentorSchema.methods.createPasswordResetToken = function() {
-    const resetToken = crypto.randomBytes(32).toString('hex');
-    this.passwordResetToken = crypto
-        .createHash('sha256')
-        .update(resetToken)
-        .digest('hex');
-    this.passwordResetExpires = Date.now() + 5 * 60 * 1000;
-    return resetToken;
-};
-//-------------------Document Middleware-----------------//
-mentorSchema.pre('save', function(next) {
-    if (this.isNew) return next();
-    this.onboarding_completed = true;
-    next();
 });
-
-mentorSchema.pre('save', async function(next) {
-    // Only run this function only when password got modified (or created)
-    if (!this.isModified('pass')) return next();
-    this.pass = await bcrypt.hash(this.pass, 12);
-    this.passConfirm = undefined;
-});
-//-------------------Query Middleware-------------------//
-mentorSchema.pre(/^find/, function(next) {
-    this.select(
-        'photo name email about courses onboarding_completed active role skill'
-    );
-    // this.find({ active: { $ne: false } });
+//-------------------Query Middleware----------------//
+meetingsSchema.pre(/^find/, function(next) {
+    this.select('mentor user status scheduledDate');
     next();
 });
 //-------------------------Export-----------------------//
-const Mentor = mongoose.model('Mentor', mentorSchema);
-module.exports = Mentor;
+const Meeting = mongoose.model('Meeting', meetingsSchema);
