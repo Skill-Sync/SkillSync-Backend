3b460c163ef393e606ae8e40d1d6901c26da1f6b
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-09-15 16:41:58 +0300

    adding addUserToMeeting

--- src/utils/dyte.js
+++ src/utils/dyte.js
@@ -1,15 +1,12 @@
 const axios = require('axios').default;
 
-const DYTE_API_Meeting_URL = 'https://api.dyte.io/v2/meetings';
-const DYTE_API_Adding_URL = `https://api.dyte.io/v2
-/meetings/{meeting_id}/participants`;
-
+const DYTE_API_BASE_URL = 'https://api.dyte.io/v2';
 const DYTE_API_TOKEN = `${process.env.ORG_ID}:${process.env.API_KEY}`;
 
-async function createDyteMeeting(title) {
+exports.createDyteMeeting = async function(title) {
     try {
         const response = await axios.post(
-            DYTE_API_Meeting_URL,
+            `${DYTE_API_BASE_URL}/meetings`,
             {
                 title: `${title}`,
                 preferred_region: 'ap-south-1',
@@ -29,14 +26,28 @@ async function createDyteMeeting(title) {
         console.error('Error creating Dyte meeting:', error);
         throw error;
     }
-}
+};
 
-// Example usage:
-const meetingTitle = 'My Dyte Meeting';
-createDyteMeeting(meetingTitle)
-    .then(meetingId => {
-        console.log('Created Dyte Meeting with ID:', meetingId);
-    })
-    .catch(error => {});
+exports.addUserToMeeting = async function(meetingId, userData) {
+    try {
+        const url = `${DYTE_API_BASE_URL}/meetings/${meetingId}/participants`;
+        const response = await axios.post(url, userData, {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Basic ${DYTE_API_TOKEN}`
+            }
+        });
 
-//add participant -->auth token
+        return response.data.token;
+    } catch (error) {
+        console.error('Error adding user to Dyte meeting:', error);
+        throw error;
+    }
+};
+
+const userData = {
+    name: '',
+    picture: '',
+    preset_name: '',
+    custom_participant_id: ''
+};
