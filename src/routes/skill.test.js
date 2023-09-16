2fd725ba84935578692c8dd89c6d110d793c14a0
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-09-16 23:46:40 +0300

    Final commit before Phase 3 submission

--- src/routes/skill.test.js
+++ src/routes/skill.test.js
@@ -2,8 +2,8 @@ const request = require('supertest');
 const app = require('../app'); // Import your Express app
 const { mongoConnect, mongoDisconnect } = require('../utils/mongoDB');
 //--------------------------------//
-const adminAccessToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYWNjZXNzIiwiaWQiOiI2NTA0YmRlYWExZjliMTU1NGI5YjAwNjUiLCJ1c2VyVHlwZSI6InVzZXIiLCJpYXQiOjE2OTQ4OTE1NDAsImV4cCI6MTY5NDg5MjQ0MH0.CNrsSxPBCJ3NveCQUyRolfBdbHws3H6suy4SH74lAls`;
-const adminRefreshToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoicmVmcmVzaCIsImlkIjoiNjUwNGJkZWFhMWY5YjE1NTRiOWIwMDY1IiwidXNlclR5cGUiOiJ1c2VyIiwicmVmcmVzaFNlc3Npb24iOiI2NTA1ZmUxNGE5MDFjNGQ4YzUwOGFjMWYiLCJpYXQiOjE2OTQ4OTE1NDAsImV4cCI6MTY5NTQ5NjM0MH0.g-PRJ_sv3hjCQqGzFaLpsnzkmVIuOnkzRTYURiqE_3A`;
+const adminAccessToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYWNjZXNzIiwiaWQiOiI2NTA0YmRlYWExZjliMTU1NGI5YjAwNzYiLCJ1c2VyVHlwZSI6ImFkbWluIiwiaWF0IjoxNjk0ODk2NDk2LCJleHAiOjE2OTQ4OTczOTZ9.V5pNXrzSQ60tcFFO9MNTq9ip9YqEDtB9kAqTuVydBWk`;
+const adminRefreshToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoicmVmcmVzaCIsImlkIjoiNjUwNGJkZWFhMWY5YjE1NTRiOWIwMDc2IiwidXNlclR5cGUiOiJhZG1pbiIsInJlZnJlc2hTZXNzaW9uIjoiNjUwNjExNzBmM2Q5NjI3ZGJjNDY0ZWRmIiwiaWF0IjoxNjk0ODk2NDk2LCJleHAiOjE2OTU1MDEyOTZ9.hG9PFzyblPOQnTPvZbxgZzSyHF-u-ejPbQnezD1-QRU`;
 //--------------------------------//
 beforeAll(async () => {
     await mongoConnect();
